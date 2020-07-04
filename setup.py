d21cbc51d25692b2b94434f26cc3c00eccc4a0f2
Gonzalo Zigaran <gjzigaran@gmail.com>
2020-07-04 13:12:44 -0300

    package_data

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     url="https://github.com/gonzigaran/folpy",
-    packages=['folpy'],
+    packages=setuptools.find_packages(),
     package_data={'folpy': ['utils/minion/minion']},
     classifiers=[
         "Programming Language :: Python :: 3",
