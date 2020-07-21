85f5346ea82d3a57e8951a0db146c09ce86d72c4
Gonzalo Zigaran <gjzigaran@gmail.com>
2020-07-20 23:46:59 -0300

    parser

--- folpy/utils/parser/parser.py
+++ folpy/utils/parser/parser.py
@@ -2,14 +2,13 @@
 # !/usr/bin/env python
 
 import sys
-import gzip
 
 from ...semantics import Model, Algebra
 from ...semantics.modelfunctions import Relation, Operation
 from ...syntax import Type, AlgebraicType
-from ...syntax.formulas import Formula
-from ...syntax.terms import Variable, Term
-from . import preprocessing
+
+
+STATES = ['UNIVERSE', 'RELATION', 'OPERATION', 'NAME']
 
 
 class ParserError(Exception):
@@ -21,221 +20,179 @@ class ParserError(Exception):
         super(ParserError, self).__init__(("Line %s: " % line) + message)
 
 
-def c_input(line):
+class Parser(object):
     """
-    Clean input
+    Parse a model
+
+    >>> from folpy.semantics import Algebra, Model
+    >>> p = Parser("folpy/utils/parser/example.model")
+    >>> type(p.parse()) == Model
+    True
     """
-    line = line.decode("ascii")
-    if "#" in line:
-        line = line[:line.find("#")]
-    return line.strip()
 
+    def __init__(self, path=None, preprocess=True, verbose=True):
+        if path:
+            try:
+                f = open(path, "rb")
+            except FileNotFoundError:
+                raise ParserError(-1, "File missing")
+        else:
+            f = sys.stdin
+        self.file = f
+        self.preprocess = preprocess
+        self.verbose = verbose
+        self.name_type = ({}, {})
+        self.relations = {}
+        self.operations = {}
+        self.current_state = 'UNIVERSE'
+        self.current_sym = None
+        self.current_len = 0
+        self.universe = None
+        self.current_line = 0
+        self.name = None
 
-def parse_universe(line):
-    # el universo puede estar hecho de strings, de tuplas,etc
-    return [eval(i) for i in line.split()]
+    def parse(self):
+        for linenumber, line in enumerate(self.file):
+            assert self.current_line == linenumber
+            line = line.decode("ascii").strip()
+            if ("#" not in line) and (line != ""):
+                self.parse_line(line)
+            self.current_line += 1
+        return self.return_model()
 
+    def parse_line(self, line):
+        if self.current_state is None:
+            if self.current_len != 0:
+                raise ParserError(self.current_line,
+                                  "Bad definition of operation or relation")
+            self.current_state = self.get_state(line)
+        if self.current_state == 'UNIVERSE':
+            self.parse_universe(line)
+        elif self.current_state == 'RELATION':
+            self.parse_relation(line)
+        elif self.current_state == 'OPERATION':
+            self.parse_operation(line)
+        elif self.current_state == 'NAME':
+            self.parse_name(line)
+        else:
+            raise ParserError(self.current_line,
+                              "State error, %s" % self.current_state)
 
-def barajador(t, d1, d2):
-    result = list(d1)
-    for i, k in enumerate(d2):
-        for j in (j for j, x in enumerate(d1) if x == k):
-            result[j] = t[i]
-    return tuple(result)
+    def get_state(self, line):
+        assert self.current_state is None
+        line_list = line.split()
+        if (not line_list[0].isdigit()) and len(line_list) == 3:
+            return 'RELATION'
+        elif (not line_list[0].isdigit()) and len(line_list) == 2:
+            return 'OPERATION'
+        else:
+            return None
 
+    def parse_universe(self, line):
+        assert self.universe is None
+        self.universe = [eval(i) for i in line.split()]
+        self.current_state = None
 
-def parse_defformula(line, universe, relations, operations):
-    # R(x,y) m(x,y) == j(x,y)
-    # print("%s interpreted as:" % line)
-    entorno = dict()
-    entorno["model"] = Model(universe, relations, operations)
-    entorno.update({r: relations[r].syntax_sym for r in relations})
-    entorno.update({f: operations[f].syntax_sym for f in operations})
-    sym, line = line.split("(", 1)
-    declaracion, formula = line.split(")", 1)
-    declaracion = declaracion.split(",")
-    if len(declaracion) > len(set(declaracion)):
-        raise ValueError("por ahora no se pueden declarar repitiendo variables")
-    declaracion = tuple(Variable(v) for v in declaracion)
+    def parse_relation(self, line):
+        line_list = line.split()
+        if line_list[0].isdigit():
+            if not all(value.isdigit() for value in line_list):
+                raise ParserError(self.current_line,
+                                  "Bad definition of relation tuple")
+            self.parse_relation_tuple(line_list)
+        else:
+            assert len(line_list) == 3
+            self.parse_relation_name(line_list)
 
-    vars = {v.sym: v for v in declaracion}
-    entorno.update(vars)
-    try:
-        formula = eval(formula, globals(), entorno)
-    except NameError:
-        raise ValueError("Missing variables in the declaration of %s" % sym)
-    if set(formula.free_vars()) > set(declaracion):
-        raise ValueError("Missing variables in the declaration of %s" % sym)
-    if isinstance(formula, Formula):
-        entorno["formula"] = formula
-        entorno["arity"] = len(vars)
+    def parse_relation_name(self, line_list):
+        rel_name = line_list[0]
+        rel_len = int(line_list[1])
+        rel_arity = int(line_list[2])
+        if rel_name in self.name_type[1].keys():
+            raise ParserError(self.current_line,
+                              "Relation name %s repeted" % rel_name)
+        self.name_type[1][rel_name] = rel_arity
+        self.relations[rel_name] = []
+        self.current_sym = rel_name
+        self.current_len = rel_len
 
-        valores = eval("formula.extension(model,arity)", globals(), entorno)
-        valores = {barajador(t,
-                             declaracion,
-                             formula.implied_declaration()) for t in valores}
-        result = Relation(sym, len(declaracion))
-        result.r = valores
-        # print(valores)
-        return result
-    elif isinstance(formula, Term):
-        raise NotImplementedError("Functions declared by\
-             formula not implemented")
+    def parse_relation_tuple(self, line_list):
+        rel_name = self.current_sym
+        rel_len = self.current_len
+        rel_arity = self.name_type[1][rel_name]
+        if len(line_list) != rel_arity:
+            raise ParserError(self.current_line,
+                              "Relation %s arity doesn't match" % rel_name)
+        tuple_value = tuple([int(val) for val in line_list])
+        self.relations[rel_name].append(tuple_value)
+        self.current_len = rel_len - 1
+        if self.current_len == 0:
+            self.current_state = None
+            self.current_sym = None
 
+    def parse_operation(self, line):
+        line_list = line.split()
+        if line_list[0].isdigit():
+            if not all(value.isdigit() for value in line_list):
+                raise ParserError(self.current_line,
+                                  "Bad definition of operation tuple")
+            self.parse_operation_tuple(line_list)
+        else:
+            assert len(line_list) == 2
+            self.parse_operation_name(line_list)
 
-def parse_defrel(line):
-    sym, ntuples, arity = line.split()
-    ntuples, arity = int(ntuples), int(arity)
-    return Relation(sym, arity), ntuples
+    def parse_operation_name(self, line_list):
+        op_name = line_list[0]
+        op_arity = int(line_list[1])
+        if op_name in self.name_type[0].keys():
+            raise ParserError(self.current_line,
+                              "Operation name %s repeted" % op_name)
+        self.name_type[0][op_name] = op_arity
+        self.operations[op_name] = {}
+        self.current_sym = op_name
+        self.current_len = len(self.universe) ** op_arity
 
+    def parse_operation_tuple(self, line_list):
+        op_name = self.current_sym
+        op_len = self.current_len
+        op_arity = self.name_type[0][op_name]
+        if len(line_list) != op_arity + 1:
+            raise ParserError(self.current_line,
+                              "Operation %s arity doesn't match" % op_name)
+        line_list = [int(val) for val in line_list]
+        tuple_value = tuple(line_list[:op_arity])
+        image = line_list[op_arity]
+        self.operations[op_name][tuple_value] = image
+        self.current_len = op_len - 1
+        if self.current_len == 0:
+            self.current_state = None
+            self.current_sym = None
 
-def parse_defop(line):
-    sym, arity = line.split()
-    arity = int(arity)
-    return Operation(sym, arity)
+    def parse_name(self, line):
+        raise NotImplementedError("Name to model not implemented")
 
-
-def parse_tuple(line):
-    return tuple(map(eval, line.split()))
-
-
-def random_delete_tuples(number):
-    def f(rel):
-        for i in range(number):
-            rel.r.pop()
-        return rel
-
-    return f
-
-
-def parser(path=None, preprocess=True, verbose=True):
-    """
-    New parser
-    """
-    if path:
-        try:
-            f = gzip.open(path, "rb")
-            f.readline()
-            f.seek(0)
-        except FileNotFoundError:
-            raise ParserError(-1, "File missing")
-    else:
-        f = sys.stdin
-    relations = {}
-    operations = {}
-    current_rel = None
-    current_op = None
-    rel_missing_tuples = 0
-    op_missing_tuples = 0
-    universe = None
-    decorators = []
-    for linenumber, line in enumerate(f):
-        assert (current_op is None or current_rel is None)
-        try:
-            line = c_input(line)
-            if line:
-                if universe is None:
-                    # tiene que ser el universo!
-                    universe = parse_universe(line)
-                elif current_rel is None and current_op is None:
-                    if "@" in line:
-                        decorators.append(eval(line[1:]))
-                    elif "(" in line:
-                        # empieza una relacion u operacion definida por formula
-                        relop = parse_defformula(line,
-                                                 universe,
-                                                 relations,
-                                                 operations)
-                        if type(relop) == Relation:
-                            for d in reversed(decorators):
-                                relop = d(relop)
-                            decorators = []
-                            relations[relop.sym] = relop
-
-                            # TODO APLICACION DE DECORADORES
-                        else:
-                            operations[relop.sym] = relop
-                            # TODO APLICACION DE DECORADORES
-                    elif line.count(" ") == 1:
-                        # empieza una operacion
-                        current_op = parse_defop(line)
-                        op_missing_tuples = len(universe) ** current_op.arity
-                        if verbose:
-                            print("universe %s" % universe)
-                            print("%s tuples: %s" %
-                                  (current_op.sym, op_missing_tuples))
-                    elif line.count(" ") == 2:
-                        # empieza una relacion
-                        current_rel, rel_missing_tuples = parse_defrel(line)
-                        if verbose:
-                            try:
-                                print("%s density: %f" % (
-                                    current_rel.sym,
-                                    float(rel_missing_tuples) /
-                                    (len(universe) ** current_rel.arity)))
-                            except ValueError:
-                                print("WARNING: no pudo calcular la densidad")
-                else:
-                    if current_rel is not None:
-                        # continua una relacion
-                        try:
-                            if rel_missing_tuples:
-                                current_rel.add(parse_tuple(line))
-                                rel_missing_tuples -= 1
-                            if not rel_missing_tuples:
-                                relations[current_rel.sym] = current_rel
-                                current_rel = None
-                                # TODO APLICACION DE DECORADORES
-                        except ValueError:
-                            print(line)
-                            raise
-                    elif current_op is not None:
-                        # continua una operacion
-                        if op_missing_tuples:
-                            current_op.add(parse_tuple(line))
-                            op_missing_tuples -= 1
-                        if not op_missing_tuples:
-                            operations[current_op.sym] = current_op
-                            current_op = None
-                            # TODO APLICACION DE DECORADORES
-        except Exception as e:
-            raise e
-            raise ParserError(linenumber, e.args[0])
-            # TODO el manejo de errores no deberia imprimir excepciones por
-            # pantalla
-    if universe is None:
-        raise ParserError(linenumber, "Universe not defined")
-    if current_rel is not None and not rel_missing_tuples:
-        raise ParserError(
-            linenumber, "Missing tuples for relation %s" % current_rel.sym)
-    if current_op is not None:
-        raise ParserError(
-            linenumber, "Missing tuples for operation %s" % current_op.sym)
-
-    if preprocess:
-        prep_relations = set()
-        for sym in relations:
-            if sym.startswith("T"):
-                rel = relations[sym]
-                union_pre = preprocessing.preprocesamiento2(rel)
-                prep_relations = prep_relations.union(union_pre)
-        relations = {sym: relations[sym]
-                     for sym in relations if not sym.startswith("T")}
-        if verbose:
-            print("Target thinning turned T into %s Ts" % len(prep_relations))
-        for r in prep_relations:
-            relations[r.sym] = r
-
-    operations_names = {key: value.arity() for key, value in operations}
-    if relations == {}:
-        algebra_type = AlgebraicType(operations_names)
-        return Algebra(algebra_type, universe, operations)
-    else:
-        relations_names = {key: value.arity() for key, value in relations}
-        model_type = Type(operations_names, relations_names)
-        return Model(model_type, universe, relations, operations)
+    def return_model(self):
+        self.operations = {k: Operation(
+            self.operations[k],
+            self.universe
+            ) for k in self.operations.keys()}
+        if self.relations == {}:
+            algebra_type = AlgebraicType(self.name_type[0])
+            return Algebra(algebra_type,
+                           self.universe,
+                           self.operations)
+        else:
+            self.relations = {k: Relation(
+                self.relations[k],
+                self.universe
+                ) for k in self.relations.keys()}
+            model_type = Type(self.name_type[0], self.name_type[1])
+            return Model(model_type,
+                         self.universe,
+                         self.operations,
+                         self.relations)
 
 
 if __name__ == "__main__":
-    MODEL = parser()
-    print(MODEL)
+    import doctest
+    doctest.testmod()
