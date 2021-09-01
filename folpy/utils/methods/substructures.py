9c16076dffc1d14cefb3b81a7ee555841bd4bd7c
Gonzalo Zigaran <gjzigaran@gmail.com>
2021-09-01 19:50:15 -0300

    cambios en substructures

--- folpy/utils/methods/substructures.py
+++ folpy/utils/methods/substructures.py
@@ -3,9 +3,8 @@ from itertools import combinations
 from .subuniverses import subuniverses, is_subuniverse
 
 
-def substructures(
+def substructures_downup(
         model,
-        supermodel=None,
         filter_isos=False,
         filter_subdirect=False,
         subtype=None,
@@ -19,16 +18,16 @@ def substructures(
 
     >>> from folpy.examples.lattices import *
     >>> one_op_subtype = rhombus.type.subtype(["^"],[])
-    >>> empty_subtype = rhombus.type.subtype([],[])
-    >>> len(list(substructures(rhombus, rhombus.type)))
+    >>> empty = rhombus.type.subtype([],[])
+    >>> len(list(substructures_downup(rhombus, rhombus.type)))
     11
-    >>> len(list(substructures(gen_chain(4), rhombus.type)))
+    >>> len(list(substructures_downup(gen_chain(4), rhombus.type)))
     14
-    >>> len(list(substructures(rhombus, subtype=one_op_subtype)))
+    >>> len(list(substructures_downup(rhombus, subtype=one_op_subtype)))
     12
-    >>> len(list(substructures(rhombus, subtype=empty_subtype)))
+    >>> len(list(substructures_downup(rhombus, subtype=empty)))
     14
-    >>> len(list(substructures(rhombus, subtype=empty_subtype, proper=False)))
+    >>> len(list(substructures_downup(rhombus, subtype=empty, proper=False)))
     15
     """
     if not subtype:
@@ -39,14 +38,59 @@ def substructures(
             yield model.substructure(sub, subtype=subtype)
 
 
-def substructures_by_maximals(
+def substructures_updown(
         model,
-        supermodel=None,
-        filter_isos=True,
+        filter_isos=False,
         filter_subdirect=False,
         subtype=None,
         proper=True,
         without=[]):
+    """
+    Generador de subestructuras que filtra por isomorfismos y tiene opcion
+    para quedarse solo con los subdirectos (para esto el modelo tiene que ser
+    un producto)
+
+    TODO implementar lo de filter_subdirect
+    TODO implementar lo de subtype
+    TODO implementar lo de without
+    """
+    if filter_subdirect:
+        assert len(model.factors) > 1
+    if not subtype:
+        subtype = model.type
+    universe = model.universe.copy()
+    result = []
+    # conviene guardar hashes de estructuras
+    result_compl = []
+    for i in range(1, len(model)):
+        for subset in combinations(universe, i):
+            subset = set(subset)
+            possible_subuniverse = [x for x in universe if x not in subset]
+            if is_subuniverse(model, possible_subuniverse):
+                substructure = model.restrict(possible_subuniverse)
+                result_compl.append(subset)
+                if filter_isos and any(substructure.is_isomorphic(x)
+                                       for x in result):
+                    continue
+                result.append(substructure)
+                yield substructure
+    if not proper:
+        not_proper_sub = model.restrict(model.universe)
+        if not_proper_sub not in result:
+            result.append(not_proper_sub)
+            yield not_proper_sub
+    return result
+
+
+def substructures_by_maximalsss(
+        model,
+        supermodel=None,
+        filter_isos=False,
+        filter_subdirect=False,
+        subtype=None,
+        proper=True,
+        without=[],
+        known_substructures=[]):
     """
     Generador de subestructuras que filtra por isomorfismos y tiene opcion
     para quedarse solo con los subdirectos (para esto el modelo tiene que ser
@@ -63,7 +107,8 @@ def substructures_by_maximals(
     if not subtype:
         subtype = model.type
     universe = model.universe.copy()
-    result = []
+    result = known_substructures
+    # conviene guardar hashes de estructuras
     result_compl = []
     for i in range(1, len(model)):
         has_subs_of_this_len = False
@@ -87,12 +132,9 @@ def substructures_by_maximals(
                                 filter_isos=filter_isos,
                                 filter_subdirect=filter_subdirect,
                                 subtype=subtype,
-                                proper=proper):
-                    if filter_isos and any(sub.is_isomorphic(x)
-                                           for x in result):
-                        continue
+                                proper=proper,
+                                known_substructures=result):
                     if sub not in result:
-                        result.append(sub)
                         yield sub
         if not has_subs_of_this_len:
             break
@@ -102,3 +144,71 @@ def substructures_by_maximals(
             result.append(not_proper_sub)
             yield not_proper_sub
     return result
+
+
+def substructures_by_maximals(
+        model,
+        filter_isos=False,
+        filter_subdirect=False,
+        subtype=None,
+        proper=True,
+        without=[]):
+    """
+    Generador de subestructuras a partir de iteración de maximales.
+
+    >>> from folpy.examples.lattices import *
+    >>> len(list(substructures_by_maximals(rhombus)))
+    11
+    """
+    if filter_subdirect:
+        assert len(model.factors) > 1
+    if not subtype:
+        subtype = model.type
+
+    global complements_subsets
+    global known_substructures
+
+    def maximal_substructures(model, supermodel):
+        if not supermodel:
+            supermodel = model
+        diff_universe = set(supermodel.universe)-set(model.universe)
+        global complements_subsets
+        global known_substructures
+
+        for i in range(1, len(model)):
+            has_subs_of_this_len = False
+            for subset in combinations(model.universe, i):
+                subset = set(subset)
+                supersubset = subset.union(diff_universe)
+                if supersubset in complements_subsets:
+                    continue
+                if any(x.issubset(subset) for x in complements_subsets):
+                    continue
+                has_subs_of_this_len = True
+                possible_subuniverse = [x for x in model.universe if x not in subset]
+                if is_subuniverse(model, possible_subuniverse):
+                    new_substructure = supermodel.restrict(possible_subuniverse)
+                    complements_subsets.append(supersubset)
+                    if filter_isos and any(new_substructure.is_isomorphic(x)
+                                           for x in known_substructures):
+                        continue
+                    known_substructures.append(new_substructure)
+                    yield new_substructure
+            if not has_subs_of_this_len:
+                break
+
+    known_substructures = []
+    complements_subsets = []
+    old_substructure_list = maximal_substructures(model, model)
+    new_substructure_list = []
+
+    while old_substructure_list:
+        for sub in old_substructure_list:
+            known_substructures.append(sub)
+            yield sub
+            for maximal in maximal_substructures(sub, model):
+                new_substructure_list.append(maximal)
+        old_substructure_list = new_substructure_list.copy()
+        new_substructure_list = []
+
+    return True
--- tests/test_substructures.py
+++ tests/test_substructures.py
@@ -3,8 +3,9 @@ import random
 
 from folpy.examples import lattices, posets
 from folpy.utils.methods import (
-                                substructures_by_maximals,
-                                substructures
+                                substructures_updown,
+                                substructures_downup,
+                                substructures_by_maximals
                             )
 
 models = [
@@ -32,11 +33,27 @@ class SubstructuresTest(TestCase):
             self.without_iso(),
             msg="error in substructure without isomorphism"
             )
+        self.assertTrue(
+            self.with_iso(),
+            msg="error in substructure with isomorphism"
+            )
 
     def without_iso(self):
         result = True
         for model in random.choices(models, k=5):
-            t = len(list(substructures(model, filter_isos=False))) == \
+            t = len(list(substructures_downup(model, filter_isos=False))) == \
+                len(list(substructures_updown(model, filter_isos=False)))
+            result = result and t
+        for model in random.choices(models, k=5):
+            t = len(list(substructures_updown(model, filter_isos=False))) == \
                 len(list(substructures_by_maximals(model, filter_isos=False)))
+            result = result and t
+        return result
+
+    def with_iso(self):
+        result = True
+        for model in random.choices(models, k=5):
+            t = len(list(substructures_updown(model, filter_isos=True))) == \
+                len(list(substructures_by_maximals(model, filter_isos=True)))
             result = result and t
         return result
